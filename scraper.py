db229130c986a8f7fac50fd0c36a741215f7f10a
soliang04 <sliang19@uci.edu>
2024-04-22 17:38:05 -0700

    Testing commit

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ def scraper(url, resp):
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
+    # testing
     # Implementation required.
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
