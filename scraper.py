190df8fa2cff398dff71ab9848d84b72048942d2
soliang04 <sliang19@uci.edu>
2024-04-22 17:47:31 -0700

    hhi test commit

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ def scraper(url, resp):
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
-    # testing hiuhi
+    # testing hiiiiiiiiiiiiiiiiii
     # Implementation required.
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
